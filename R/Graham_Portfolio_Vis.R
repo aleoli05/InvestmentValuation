193d6c4abdd27c0ab12194a8b8a088b5b58b1346
aleoli05 <aleoli05@gmail.com>
2025-03-09 22:18:29 -0300

    Commit

--- R/Graham_Portfolio_Vis.R
+++
@@ -1,6 +0,0 @@
-#´ Graham_Portfolio_Vis
-#' @export
-Graham_Portfolio_Vis <- function(){
-  teste=1+1
-  print(teste)
-}
---
+++ R/Intelligent_Investor.R
@@ -0,0 +1,8 @@
+#' Intelligent_Investor
+#'
+#' @param Tickers xxxxx
+#' @export
+Intelligent_Investor <- function(Tickers){
+  x=sum(Tickers)
+  print(x)
+}
