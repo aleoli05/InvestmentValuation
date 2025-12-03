187e1c444f3035f254190b26fee7298ea59e360f
aleoli05 <aleoli05@gmail.com>
2025-12-02 23:01:52 -0300

    Commit Turnover

---
+++ R/Turnover.R
@@ -0,0 +1,350 @@
+#' Turnover
+#' It measures portfolio turnover
+#' @param Portfolios:Specify the portfolios or type 'Alls' for all.
+#' Portfolios:'MF_EQ', 'MF_MKW','MKW','ANNt_EQ','ANNt_MKW','Sharpe','MF_Sharpe','ANNt_Sharpe',
+#' Magic_EQ','Magic_MKW', 'Magic_Sharpe', 'Graham_EQ', 'Graham_MKW', 'Graham_Sharpe'
+#' @examples
+#' Turnover('Alls')
+
+#' @export
+
+Turnover<-function(Portfolio){
+
+library(dplyr)
+  if (length(Portfolio)==1){
+  if (Portfolio=='Alls'){
+      Portfolio=c('MF_EQ', 'MF_MKW','MKW','ANNt_EQ','ANNt_MKW','Sharpe','MF_Sharpe','ANNt_Sharpe',
+             'Magic_EQ','Magic_MKW', 'Magic_Sharpe', 'Graham_EQ', 'Graham_MKW', 'Graham_Sharpe')
+      load('~/Weights_MF_EQ_Horizon.rda')
+  }}
+if (length(Portfolio)==1){
+    options(warn=-1)
+    if(Portfolio=='MF_EQ'){
+    load('~/Weights_MF_EQ_Horizon.rda')
+    dados=(Weights_MF_EQ_Horizon)
+    View(Weights_MF_EQ_Horizon)
+    }
+  if(Portfolio=='MF_MKW'){
+    load('~/Weights_MF_MKW_Horizon.rda')
+    dados=Weights_MF_MKW_Horizon
+    View(Weights_MF_MKW_Horizon)
+  }
+  if(Portfolio=='MKW'){
+    load('~/Weights_MKW_Horizon.rda')
+    dados=Weights_MKW_Horizon
+    View(Weights_MKW_Horizon)
+  }
+  if(Portfolio=='ANNt_EQ'){
+    load('~/Weights_ANNt_EQ_Horizon.rda')
+    dados=Weights_ANNt_EQ_Horizon
+    View(Weights_ANNt_EQ_Horizon)
+  }
+  if(Portfolio=='ANNt_MKW'){
+    load('~/Weights_ANNt_MKW_Horizon.rda')
+    dados=Weights_ANNt_MKW_Horizon
+    View(Weights_ANNt_MKW_Horizon)
+  }
+  if(Portfolio=='Sharpe'){
+    load('~/Weights_Sharpe_Horizon.rda')
+    dados=Weights_Sharpe_Horizon
+    View(Weights_Sharpe_Horizon)
+  }
+  if(Portfolio=='MF_Sharpe'){
+    load('~/Weights_MF_Sharpe_Horizon.rda')
+    dados=Weights_MF_Sharpe_Horizon
+    View(Weights_MF_Sharpe_Horizon)
+  }
+  if(Portfolio=='ANNt_Sharpe'){
+    load('~/Weights_ANNt_Sharpe_Horizon.rda')
+    dados=Weights_ANNt_Sharpe_Horizon
+    View(Weights_ANNt_Sharpe_Horizon)
+  }
+  if(Portfolio=='Magic_EQ'){
+    load('~/Weights_Magic_EQ_Horizon.rda')
+    dados=Weights_Magic_EQ_Horizon
+    View(Weights_Magic_EQ_Horizon)
+  }
+  if(Portfolio=='Magic_MKW'){
+    load('~/Weights_Magic_MKW_Horizon.rda')
+    dados=Weights_Magic_MKW_Horizon
+    View(Weights_Magic_MKW_Horizon)
+  }
+  if(Portfolio=='Magic_Sharpe'){
+    load('~/Weights_Magic_Sharpe_Horizon.rda')
+    dados=Weights_Magic_Sharpe_Horizon
+    View(Weights_Magic_Sharpe_Horizon)
+  }
+  if(Portfolio=='Graham_EQ'){
+    load('~/Weights_Graham_EQ_Horizon.rda')
+    dados=Weights_Graham_EQ_Horizon
+    View(Weights_Graham_EQ_Horizon)
+  }
+  if(Portfolio=='Graham_MKW'){
+    load('~/Weights_Graham_MKW_Horizon.rda')
+    dados=Weights_Graham_MKW_Horizon
+    View(Weights_Graham_MKW_Horizon)
+  }
+  if(Portfolio=='Graham_Sharpe'){
+    load('~/Weights_Graham_Sharpe_Horizon.rda')
+    dados=Weights_Graham_Sharpe_Horizon
+    View(Weights_Graham_Sharpe_Horizon)
+  }
+
+  Linhas_CV=(nrow(dados)-2)
+  Linhas=seq(from=3, to=nrow(dados)-1, by=2)
+  Colunas = 50
+  Compras=matrix(nrow=Linhas_CV, ncol=Colunas)
+  Vendas=matrix(nrow=Linhas_CV, ncol=Colunas)
+  ordem=0
+  n_buys=0
+  Buy=NULL
+  Sell=NULL
+    for (i in Linhas){
+        for (j in 3:Colunas){
+          if (is.na(dados[i,j])==FALSE && is.na(dados[i+2,1])==FALSE){
+            test=dados[i+2,3:ncol(dados)]
+            excluir=names(test)[colSums(is.na(test)) > 0]
+            test <- test %>%
+              select(-all_of(excluir))
+            if (any(unlist(test)==dados[i,j])){
+                ordem=which(dados[i+2,]==dados[i,j])
+                if(as.numeric(dados[i+1,j])>as.numeric(dados[i+3,ordem])){
+                   Compras[i+1,j] = as.numeric(dados[i+1,j])-as.numeric(dados[i+3,ordem])
+                  }else{
+                     Vendas[i+1,j] = as.numeric(dados[i+3,ordem])-as.numeric(dados[i+1,j])
+                    }
+            }
+            teste=dados[i+2,3:ncol(dados)]
+            excluir=names(teste)[colSums(is.na(teste)) > 0]
+            teste <- teste %>%
+              select(-all_of(excluir))
+            if (is.na(dados[i,j])==FALSE && any(unlist(teste)==as.character(dados[i,j]))==FALSE){
+              Compras[i+1,j] = as.numeric(dados[i+1,j])
+            }
+            teste2=dados[i,3:ncol(dados)]
+            excluir=names(teste2)[colSums(is.na(teste2)) > 0]
+            teste2 <- teste2 %>%
+              select(-all_of(excluir))
+            if (is.na(dados[i+2,j])==FALSE && any(unlist(teste2)==as.character(dados[i+2,j]))==FALSE){
+              Vendas[i+1,j] = as.numeric(dados[i+1,j])
+            }
+          }
+        }
+      if(is.na(dados[i+2,1])==FALSE){
+      Compras2=Compras%>% replace(is.na(.), 0)
+      Vendas2=Vendas%>% replace(is.na(.), 0)
+      #Compras2[i+1,][is.na(Compras2[i+1])] <-0
+      n_buys=n_buys+1
+      Buy[n_buys]=sum(Compras2[i+1,])
+      Sell[n_buys]=sum(Vendas2[i+1,])
+      }
+    }
+  if(sum(Buy)>sum(Sell)){
+  Turn=Buy
+  }else{
+    Turn=Sell
+  }
+  Turn[Turn>1]=1
+  Media=round(mean(Turn),2)
+  if(Media>1){
+    Media=1
+  }
+  Turnovers=dados[,1:2]
+  posicao_=seq(from=1, to=length(Turn), by=2)
+  elemento=0
+  Lista=Turn
+  for(i in 1:length(posicao_)){
+    posicao=posicao_[i]
+  Lista= append(Lista, elemento, after = posicao)
+  }
+  elemento2=c('Turnovers',0)
+  Lista=append(Lista, elemento2, after = 0)
+  Posicao3=length(Lista)+2
+  elemento3=NA
+  for(i in length(Lista):(nrow(Turnovers)-1)){
+    posicao=posicao[i]
+    Lista= append(Lista, elemento3)
+  }
+  Lista[Posicao3-1]=0
+  Lista[Posicao3]='Mean'
+  Lista[Posicao3+1]=Media
+  Turnovers[,3]=Lista
+  Turnovers=na.omit(Turnovers)
+  View(Turnovers)
+  print(Turnovers)
+}
+
+if (length(Portfolio)>1){
+   Port=Portfolio
+
+dados=(Weights_MF_EQ_Horizon)
+Turnovers_Alls=dados[,1:2]
+Turnover_Alls[1,1]='Portfolios'
+Turnovers_Alls=na.omit(Turnovers_Alls)
+
+
+for (m in 1:length(Port)){
+  Portfolio=Port[m]
+
+  library(dplyr)
+  options(warn=-1)
+  if(Portfolio=='MF_EQ'){
+    load('~/Weights_MF_EQ_Horizon.rda')
+    dados=(Weights_MF_EQ_Horizon)
+    View(Weights_MF_EQ_Horizon)
+  }
+  if(Portfolio=='MF_MKW'){
+    load('~/Weights_MF_MKW_Horizon.rda')
+    dados=Weights_MF_MKW_Horizon
+    View(Weights_MF_MKW_Horizon)
+  }
+  if(Portfolio=='MKW'){
+    load('~/Weights_MKW_Horizon.rda')
+    dados=Weights_MKW_Horizon
+    View(Weights_MKW_Horizon)
+  }
+  if(Portfolio=='ANNt_EQ'){
+    load('~/Weights_ANNt_EQ_Horizon.rda')
+    dados=Weights_ANNt_EQ_Horizon
+    View(Weights_ANNt_EQ_Horizon)
+  }
+  if(Portfolio=='ANNt_MKW'){
+    load('~/Weights_ANNt_MKW_Horizon.rda')
+    dados=Weights_ANNt_MKW_Horizon
+    View(Weights_ANNt_MKW_Horizon)
+  }
+  if(Portfolio=='Sharpe'){
+    load('~/Weights_Sharpe_Horizon.rda')
+    dados=Weights_Sharpe_Horizon
+    View(Weights_Sharpe_Horizon)
+  }
+  if(Portfolio=='MF_Sharpe'){
+    load('~/Weights_MF_Sharpe_Horizon.rda')
+    dados=Weights_MF_Sharpe_Horizon
+    View(Weights_MF_Sharpe_Horizon)
+  }
+  if(Portfolio=='ANNt_Sharpe'){
+    load('~/Weights_ANNt_Sharpe_Horizon.rda')
+    dados=Weights_ANNt_Sharpe_Horizon
+    View(Weights_ANNt_Sharpe_Horizon)
+  }
+  if(Portfolio=='Magic_EQ'){
+    load('~/Weights_Magic_EQ_Horizon.rda')
+    dados=Weights_Magic_EQ_Horizon
+    View(Weights_Magic_EQ_Horizon)
+  }
+  if(Portfolio=='Magic_MKW'){
+    load('~/Weights_Magic_MKW_Horizon.rda')
+    dados=Weights_Magic_MKW_Horizon
+    View(Weights_Magic_MKW_Horizon)
+  }
+  if(Portfolio=='Magic_Sharpe'){
+    load('~/Weights_Magic_Sharpe_Horizon.rda')
+    dados=Weights_Magic_Sharpe_Horizon
+    View(Weights_Magic_Sharpe_Horizon)
+  }
+  if(Portfolio=='Graham_EQ'){
+    load('~/Weights_Graham_EQ_Horizon.rda')
+    dados=Weights_Graham_EQ_Horizon
+    View(Weights_Graham_EQ_Horizon)
+  }
+  if(Portfolio=='Graham_MKW'){
+    load('~/Weights_Graham_MKW_Horizon.rda')
+    dados=Weights_Graham_MKW_Horizon
+    View(Weights_Graham_MKW_Horizon)
+  }
+  if(Portfolio=='Graham_Sharpe'){
+    load('~/Weights_Graham_Sharpe_Horizon.rda')
+    dados=Weights_Graham_Sharpe_Horizon
+    View(Weights_Graham_Sharpe_Horizon)
+  }
+
+  Linhas_CV=(nrow(dados)-2)
+  Linhas=seq(from=3, to=nrow(dados)-1, by=2)
+  Colunas = 50
+  Compras=matrix(nrow=Linhas_CV, ncol=Colunas)
+  Vendas=matrix(nrow=Linhas_CV, ncol=Colunas)
+  ordem=0
+  n_buys=0
+  Buy=NULL
+  Sell=NULL
+  for (i in Linhas){
+    for (j in 3:Colunas){
+      if (is.na(dados[i,j])==FALSE && is.na(dados[i+2,1])==FALSE){
+        test=dados[i+2,3:ncol(dados)]
+        excluir=names(test)[colSums(is.na(test)) > 0]
+        test <- test %>%
+          select(-all_of(excluir))
+        if (any(unlist(test)==dados[i,j])){
+          ordem=which(dados[i+2,]==dados[i,j])
+          if(as.numeric(dados[i+1,j])>as.numeric(dados[i+3,ordem])){
+            Compras[i+1,j] = as.numeric(dados[i+1,j])-as.numeric(dados[i+3,ordem])
+          }else{
+            Vendas[i+1,j] = as.numeric(dados[i+3,ordem])-as.numeric(dados[i+1,j])
+          }
+        }
+        teste=dados[i+2,3:ncol(dados)]
+        excluir=names(teste)[colSums(is.na(teste)) > 0]
+        teste <- teste %>%
+          select(-all_of(excluir))
+        if (is.na(dados[i,j])==FALSE && any(unlist(teste)==as.character(dados[i,j]))==FALSE){
+          Compras[i+1,j] = as.numeric(dados[i+1,j])
+        }
+        teste2=dados[i,3:ncol(dados)]
+        excluir=names(teste2)[colSums(is.na(teste2)) > 0]
+        teste2 <- teste2 %>%
+          select(-all_of(excluir))
+        if (is.na(dados[i+2,j])==FALSE && any(unlist(teste2)==as.character(dados[i+2,j]))==FALSE){
+          Vendas[i+1,j] = as.numeric(dados[i+1,j])
+        }
+      }
+    }
+    if(is.na(dados[i+2,1])==FALSE){
+      Compras2=Compras%>% replace(is.na(.), 0)
+      Vendas2=Vendas%>% replace(is.na(.), 0)
+      #Compras2[i+1,][is.na(Compras2[i+1])] <-0
+      n_buys=n_buys+1
+      Buy[n_buys]=sum(Compras2[i+1,])
+      Sell[n_buys]=sum(Vendas2[i+1,])
+    }
+  }
+  if(sum(Buy)>sum(Sell)){
+    Turn=Buy
+  }else{
+    Turn=Sell
+  }
+  Turn[Turn>1]=1
+  Media=round(mean(Turn),2)
+  if(Media>1){
+    Media=1
+  }
+  Turnovers=dados[,1:2]
+  posicao_=seq(from=1, to=length(Turn), by=2)
+  elemento=0
+  Lista=Turn
+  for(i in 1:length(posicao_)){
+    posicao=posicao_[i]
+    Lista= append(Lista, elemento, after = posicao)
+  }
+  elemento2=c(Portfolio,'Turnovers')
+  Lista=append(Lista, elemento2, after = 0)
+  Posicao3=length(Lista)+2
+  elemento3=NA
+  for(i in length(Lista):(nrow(Turnovers)-1)){
+    posicao=posicao[i]
+    Lista= append(Lista, elemento3)
+  }
+  Lista[Posicao3-1]=0
+  Lista[Posicao3]='Mean'
+  Lista[Posicao3+1]=Media
+  Turnovers[,3]=Lista
+  Turnovers=na.omit(Turnovers)
+  View(Turnovers)
+  print(Turnovers)
+
+  Turnovers_Alls[,2+m]=Turnovers[,3]
+  }
+  View(Turnovers_Alls)
+  print(Turnovers_Alls)
+ }
+}
